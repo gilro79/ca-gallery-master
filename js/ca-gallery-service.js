69be3790713012aa58633797defc0927bf1907fa
Roy Gil <gilro79@gmail.com>
2021-10-18 16:31:47 +0300

    ME was edited. added service.js

---
+++ js/ca-gallery-service.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var gProjects = [
+    {
+        id: "minesweeper",
+        name: "Minesweeper",
+        title: "Curful where you click",
+        desc: "You need to flag all mines and open all the other cells. the clock is ticking",
+        url: "https://gilro79.github.io/MineSweeper/",
+        publishedAt: 1633549741000,
+        labels: ["Vintage gamse", "keyboard events"],
+    },
+    {
+        id: "pacman",
+        name: "Pacman",
+        title: "Dodge that ghosts",
+        desc: "eat your food and don't get eaten",
+        url: "https://gilro79.github.io/pacman/",
+        publishedAt: 1634574558000,
+        labels: ["Vintage gamse", "keyboard events"],
+    },
+]
